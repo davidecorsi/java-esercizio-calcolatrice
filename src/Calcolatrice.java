8ec452ff6369c79c7cf3c77f2c81314a9fb14ce6
Davide Corsi <davide.corsi@par-tec.it>
2021-10-13 22:06:49 +0200

    first commit

---
+++ src/Calcolatrice.java
@@ -0,0 +1,7 @@
+/*
+ * Creare una classe calcolatrice che implementi i 4 metodi principali: addizione, sottrazione,
+ * moltiplicazione, divisione.
+ */
+public class Calcolatrice {
+
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+/*
+ * Creare una classe main che utilizza i 4 metodi della calcolatrice.
+ */
+public class Main {
+	public static void main(String[] args) {
+	}
+}
