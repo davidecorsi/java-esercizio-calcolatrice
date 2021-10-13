c7ddb7221cc179aa9576a5943a4fb38e1eafe617
Davide Corsi <davide.corsi@par-tec.it>
2021-10-13 22:07:17 +0200

    risoluzione

--- src/Calcolatrice.java
+++ src/Calcolatrice.java
@@ -3,5 +3,20 @@
  * moltiplicazione, divisione.
  */
 public class Calcolatrice {
-
+	
+	public double addizione(double x, double y) {
+		return x + y;
+	}
+	
+	public double sottrazione(double x, double y) {
+		return x - y;
+	}
+	
+	public double moltiplicazione(double x, double y) {
+		return x * y;
+	}
+	
+	public double divisione(double x, double y) {
+		return x / y;
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@
  */
 public class Main {
 	public static void main(String[] args) {
+		Calcolatrice calcolatrice = new Calcolatrice();
+		System.out.println(calcolatrice.addizione(3, 2));
+		System.out.println(calcolatrice.sottrazione(3, 2));
+		System.out.println(calcolatrice.moltiplicazione(4, 5));
+		System.out.println(calcolatrice.divisione(15, 3));
 	}
 }
